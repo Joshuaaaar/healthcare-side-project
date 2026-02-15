6f0116e8889a133d9fc5ceceb40046900f829ff9
Joshua Ramnauth <joshua20050710@gmail.com>
2026-02-14 22:06:18 -0500

    a

--- backend/main.py
+++ backend/main.py
@@ -3,6 +3,16 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/Health")
+@app.get("/health")
 async def  health_check():
-    return {"status" : "healthu"}
+    return {"status" : "healthy"}
+
+
+@app.post("/scan")
+async def scan():
+    return {
+        
+        "diagnosis" : 'Healthy', 
+        "confidence" : 0.93
+        
+    }
