707749654bad6e581204939e6faa21e7182db059
Joshua Ramnauth <joshua20050710@gmail.com>
2026-02-16 21:52:47 -0500

    created an updated app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,35 @@
 
-function App(){
-  
-  return <div>< Message /></div>;
+
+function App() {
+
+
+  return (
+
+    <div>
+      <header>
+
+      <h1>Nail Scanner App</h1>
+
+      </header>
+
+      <main>
+
+      <h2>Upload a picture of your Nail</h2>
+
+      </main>
+
+
+      <footer>
+
+        <p>© 2026 Nail AI</p>
+        
+      </footer>
+
+
+    </div>
+
+  )
+
 
 }
 
